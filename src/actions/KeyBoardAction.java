df95af3ca83cc068d5c07b732818f8238cd5b8e1
amarsurya123 <109359345+amarsurya123@users.noreply.github.com>
2022-07-15 19:32:50 +0530

    It is Changed by Amar

--- src/actions/KeyBoardAction.java
+++ src/actions/KeyBoardAction.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class KeyBoardAction {
-
+//Change by Amar
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "D:\\Software Testing\\Selenium\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
